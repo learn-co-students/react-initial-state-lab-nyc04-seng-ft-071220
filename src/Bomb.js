389281ff56741d59175b0ee109d4a91dcbf18425
wilsonng21 <wilsongch92@gmail.com>
2020-10-05 09:28:08 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+        return (
+            <div>{message}</div>
+        )
+    
+    }
+    
+}
+
+export default Bomb
+
+// Its initial state should have a property called currentSlideIndex that starts at 0.
+// It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex
